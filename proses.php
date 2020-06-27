14e7ee964a2dbf408f749fb3a1ba797b816f9d75
RoroAryani <62755787+RoroAryani@users.noreply.github.com>
2020-06-27 21:37:34 +0700

    Add files via upload

---
+++ proses.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Validasi Data</title>
+</head>
+<body>
+	<center><h1>Validasi Data Mahasiswa</h1></center>
+	<h3>Selamat datang</h3> <br>
+	Data : <?php echo $_POST["status"];?><br>
+	Nama : <?php echo $_POST["nama"];?><br>
+	NIM  : <?php echo $_POST["nim"]; ?><br>
+	Kelas:  <?php echo $_POST["Kelas"]; ?><br>
+	Jenis kelamin  : <?php echo $_POST["jeniskelamin"];?><br>
+	Alamat : <?php echo $_POST["alamat"]; ?><br>
+</body>
+</html>
